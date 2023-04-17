c7cc3493b478099428b2f155f7752e7d34340901
Abhinay Pandey <abhinaypandey02@gmail.com>
2023-04-17 01:11:42 +0000

    nunito font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from 'next/app'
 
 import '../styles/globals.css'
+import { Nunito_Sans } from 'next/font/google'
 
+// If loading a variable font, you don't need to specify the font weight
+const nunito = Nunito_Sans({ weight:['300'], subsets: ['latin'] })
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <main className={nunito.className}>
+    <Component {...pageProps} />
+  </main>
 }
 
 export default MyApp
